6366305a1dd8c6afbab11e6d7e19868501f2bfa5
QuimTorra <q.torra@gmail.com>
2022-02-06 17:24:42 +0100

    Get Endpoints

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { initializeApp } from "firebase/app"
-import { collection, getDocs, getFirestore, query, where } from "firebase/firestore"
+import { collection, getDocs, getDocsFromCache, getFirestore, query, where } from "firebase/firestore"
 import { firebaseConfig } from "./firebaseConfig.js"
 
 const fApp = initializeApp(firebaseConfig)
@@ -8,40 +8,45 @@ const db = getFirestore()
 const app = express()
 const PORT = process.env.PORT || 8080
 
-const serverRef = collection(db, "server")
+const serverRef = collection(db, "servers")
 
 // GET
 // Everything
-app.get('/:lang', async (req, res) => {
-  const q = query(serverRef, where("language", "==", req.params.lang.toLowerCase()))
+app.get('/native/:nat', async (req, res) => {
+  console.log(`On native: ${req.params.nat}`)
+  const q = query(serverRef, where("native", "==", req.params.nat.toLowerCase()), where("verified", "==", true))
   const querySnapshot = await getDocs(q)
   let docs = []
   querySnapshot.forEach((doc) => {
-
-    console.log(doc.id,  doc.data())
+    docs.push(doc.data())
   })
-  res.send(querySnapshot)
+  res.send(docs)
 })
 
-// Only SERVERS
-app.get('/server/:lang', (req, res) => {
-  res.send(`Servers are working for language ${req.params.lang}`)
+app.get('/:lang', async (req, res) => {
+  const q = query(serverRef, where("language", "==", req.params.lang.toLowerCase()), where("verified", "==", true))
+  const querySnapshot = await getDocs(q)
+  let docs = []
+  querySnapshot.forEach((doc) => {
+    docs.push(doc.data())
+  })
+  res.send(docs)
 })
 
-// Only BOTS
-app.get('/bot/:lang', (req, res) => {
-  res.send(`Bots also work for language ${req.params.lang}`) 
+app.get('/:lang/:native', async (req, res) => {
+  const q = query(serverRef, where("language", "==", req.params.lang.toLowerCase()), where("native", "==", req.params.native.toLowerCase()), where("verified", "==", true))
+  const querySnapshot = await getDocs(q)
+  let docs = []
+  querySnapshot.forEach((doc) => {
+    docs.push(doc.data())
+  })
+  res.send(docs)
 })
 
 // POST
 // Server
 app.post('/server/:lang', (req, res) => {
   res.send(`New Server listed for language ${req.params.lang}`)
-})
-
-// Bot
-app.post('/bot/:lang', (req, res) => {
-  res.send(`New Bot listed for language ${req.params.lang}`)
 })
 
 app.listen(PORT, () => console.log(`API working on port ${PORT}`))
