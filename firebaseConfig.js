34f849aa7532ddd187ff69d1b55cf2e94ae18667
QuimTorra <q.torra@gmail.com>
2022-02-06 22:46:06 +0100

    error solve

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBZbJnW5rd5V_76GD0tpFJINefYaRwFihg",
+  authDomain: "discordlanguagelearn.firebaseapp.com",
+  projectId: "discordlanguagelearn",
+  storageBucket: "discordlanguagelearn.appspot.com",
+  messagingSenderId: "737081560210",
+  appId: "1:737081560210:web:ac6987e5e8f52ff363ea4b",
+  measurementId: "G-7JCKX66B4C"
+}
